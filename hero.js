e1b75723f8f390df81b8271fd9737603989de9aa
Glen Young <extractofcactus@icloud.com>
2017-07-28 12:21:02 +0100

    constructor files started

---
+++ hero.js
@@ -0,0 +1,8 @@
+var Hero = function(name, health, favFood) {
+  this.name = name;
+  this.health = health;
+  this.favFood = favFood;
+  this.tasks = [];
+}
+
+module.exports = Hero;
---
+++ task.js
@@ -0,0 +1,8 @@
+var Task = function(difficulty, urgency, reward){
+  this.difficulty = difficulty;
+  this.urgency = urgency;
+  this.reward = reward;
+  this.complete = false;
+}
+
+module.exports = Task;
