3a1b4d54a0610efb07fb935fab4276a3a8a4b8b6
Glen Young <extractofcactus@icloud.com>
2017-07-28 13:10:37 +0100

    task has difficulty level passed

--- specs/task_spec.js
+++ specs/task_spec.js
@@ -3,4 +3,12 @@ var Task = require("../task");
 
 describe("Task", function() {
   var task;
+
+  beforeEach(function() {
+    task = new Task(3, "low", 50);
+  })
+
+  it("should have difficulty level", function() {
+    assert.strictEqual(task.difficulty, 3);
+  })
 })
