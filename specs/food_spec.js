361d859bb57b1de83ef8229543f83d9de1b83d16
Glen Young <extractofcactus@icloud.com>
2017-07-28 13:20:48 +0100

    food has name passed

--- specs/food_spec.js
+++ specs/food_spec.js
@@ -3,4 +3,12 @@ var Food = require("../food");
 
 describe("Food", function() {
   var food;
+
+  beforeEach(function() {
+    food = new Food("Chicken", 20);
+  })
+
+  it("should have name", function() {
+    assert.strictEqual(food.name, "Chicken");
+  })
 })
