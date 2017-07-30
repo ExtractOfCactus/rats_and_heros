1c5e2bb631219a7deff43f112bf319df5442da69
Glen Young <extractofcactus@icloud.com>
2017-07-30 13:52:28 +0100

    monster has weapon passed

--- specs/monster_spec.js
+++ specs/monster_spec.js
@@ -3,15 +3,19 @@ var Monster = require("../monster");
 var Weapon = require("../weapon");
 
 describe("Monster", function() {
-  var weapon;
+  var weapon1;
   var monster;
 
   beforeEach(function() {
-    weapon = new Weapon("Bronze Shortsword", 10, false);
-    monster = new Monster("Goblin", weapon, 50);
+    weapon1 = new Weapon("Bronze Shortsword", 10, false);
+    monster = new Monster("Goblin", weapon1, 50);
   })
 
   it("should have a type", function() {
     assert.strictEqual(monster.type, "Goblin");
+  })
+
+  it("should have a weapon", function() {
+    assert.deepEqual(monster.weapon, weapon1)
   })
 })
